20cae796bdc4950c5ab1cf29a307bb05413bb22c
Mark Leusink <m.leusink@gmail.com>
2015-03-05 14:44:58 +0100

    Fixes #21 (error when loading modal)

--- client/sampler/modal.js
+++ client/sampler/modal.js
@@ -1,45 +1,61 @@
-var app = angular.module('xcontrols');
 
-app.controller('ModalDemoCtrl', function ($scope, $modal, $log) {
+var modalModule = function() {
 
-  $scope.items = ['item1', 'item2', 'item3'];
+  var app = angular.module('xcontrols');
 
-  $scope.open = function (size) {
+  app.controller('ModalDemoCtrl', function ($scope, $modal, $log) {
 
-    var modalInstance = $modal.open({
-      templateUrl: 'myModalContent.html',
-      controller: 'ModalInstanceCtrl',
-      size: size,
-      resolve: {
-        items: function () {
-          return $scope.items;
+    $scope.items = ['item1', 'item2', 'item3'];
+
+    $scope.open = function (size) {
+
+      var modalInstance = $modal.open({
+        templateUrl: 'myModalContent.html',
+        controller: 'ModalInstanceCtrl',
+        size: size,
+        resolve: {
+          items: function () {
+            return $scope.items;
+          }
         }
-      }
-    });
+      });
 
-    modalInstance.result.then(function (selectedItem) {
-      $scope.selected = selectedItem;
-    }, function () {
-      $log.info('Modal dismissed at: ' + new Date());
-    });
-  };
-});
+      modalInstance.result.then(function (selectedItem) {
+        $scope.selected = selectedItem;
+      }, function () {
+        $log.info('Modal dismissed at: ' + new Date());
+      });
+    };
+  });
 
-// Please note that $modalInstance represents a modal window (instance) dependency.
-// It is not the same as the $modal service used above.
+  // Please note that $modalInstance represents a modal window (instance) dependency.
+  // It is not the same as the $modal service used above.
 
-app.controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
+  app.controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
 
-  $scope.items = items;
-  $scope.selected = {
-    item: $scope.items[0]
-  };
+    $scope.items = items;
+    $scope.selected = {
+      item: $scope.items[0]
+    };
 
-  $scope.ok = function () {
-    $modalInstance.close($scope.selected.item);
-  };
+    $scope.ok = function () {
+      $modalInstance.close($scope.selected.item);
+    };
 
-  $scope.cancel = function () {
-    $modalInstance.dismiss('cancel');
-  };
-});
+    $scope.cancel = function () {
+      $modalInstance.dismiss('cancel');
+    };
+  });
+
+};
+
+//only load the module after the HTML imports have completed
+var hasNativeHTMLImportsSupport = ('import' in document.createElement('link'));
+
+if (hasNativeHTMLImportsSupport) {
+  modalModule.call();
+} else {
+  window.addEventListener('HTMLImportsLoaded', function(e){ 
+    modalModule.call();
+  });
+}
