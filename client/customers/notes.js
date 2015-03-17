9f4c54c804a4c36b0a85bd731f3e8165ba1c40bb
Mark Leusink <m.leusink@gmail.com>
2015-03-17 11:31:27 +0100

    example added of HTML field type

--- client/customers/notes.js
+++ client/customers/notes.js
@@ -3,7 +3,7 @@ xcomponents.modelName = 'Note';
 xcomponents.fields = [
 		{ field : 'subject', required : true }, 
 		{ field : 'date', type :'date', required : true, 'default' : 'now' },
-		{ field : 'details', type : 'multiline' },
+		{ field : 'details', type : 'html', label : null }
 	];
 
 xcomponents.imageBase = 'http://demo.linqed.eu/unplugged/xcontrols-latest.nsf/';
